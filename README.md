43b3251dd8d6dcc0759379561d038eb86b2cf758
Inigo Gonzalez Vazquez <ingonza85@gmail.com>
2015-10-20 23:21:14 +0200

    Documentation and improvements

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # tag2name
+
+### Organize your music automatically with mp3 tags
+
+With tag2name you can organize and rename your mp3 files using its id3 tag data.
+
+Requirements
+------------
+python v 2.7 +
+
+Usage
+-----
+
+python main.py [-r] [-i path_to_input_folder] [-o path_to_output_folder]
+
+##### Optional parameters
+-  -r: if we want to rename the file with the title id3tag
+-  -i: an optional input folder should be specified (it will use tag2name/input if no folder is specified)
+-  -o: an optional output folder should be specified (it will use tag2name/output if no folder is specified)
--- main.py
+++ main.py
@@ -10,46 +10,50 @@ INPUT_DIR = os.path.join(BASE_DIR, 'input')
 OUTPUT_DIR = os.path.join(BASE_DIR, 'output')
 
 if __name__ == "__main__":
+    rename = False
     if len(sys.argv) > 1:
-        INPUT_DIR = sys.argv[1]
-    if len(sys.argv) > 2:
-        OUTPUT_DIR = sys.argv[2]
+        i = 0
+        for arg in sys.argv:
+            if arg == '-i':
+                INPUT_DIR = sys.argv[i + 1]
+            if arg == '-o':
+                OUTPUT_DIR = sys.argv[i + 1]
+            if arg == '-r':
+                rename = True
+            i += 1
 
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
     mp3_files = [f for f in os.listdir(INPUT_DIR) if os.path.isfile(os.path.join(INPUT_DIR,f))]
-    for mp3_path in mp3_files:
-        try:
-            _path = os.path.join(INPUT_DIR, mp3_path)
-            mp3_file = Mp3AudioFile(path=_path)
+    for mp3_name in mp3_files:
+        if '.mp3' in mp3_name:
+            try:
+                _path = os.path.join(INPUT_DIR, mp3_name)
+                mp3_file = Mp3AudioFile(path=_path)
 
-            _temp_dir = OUTPUT_DIR
+                _temp_dir = OUTPUT_DIR
 
-            new_name = ''
+                new_name = mp3_name
 
-            if mp3_file.tag.album and mp3_file.tag.album != '':
-                # new_name += mp3_file.tag.album + '-'
-                _temp_dir = os.path.join(_temp_dir, mp3_file.tag.album)
+                if mp3_file.tag.album and mp3_file.tag.album != '':
+                    _temp_dir = os.path.join(_temp_dir, mp3_file.tag.album)
 
-            if mp3_file.tag.artist and mp3_file.tag.artist != '':
-                # new_name += mp3_file.tag.artist + '-'
-                _temp_dir = os.path.join(_temp_dir, mp3_file.tag.artist)
+                if mp3_file.tag.artist and mp3_file.tag.artist != '':
+                    _temp_dir = os.path.join(_temp_dir, mp3_file.tag.artist)
 
-            if not os.path.exists(_temp_dir):
-                os.makedirs(_temp_dir)
+                if not os.path.exists(_temp_dir):
+                    os.makedirs(_temp_dir)
 
-            if mp3_file.tag.title and mp3_file.tag.title != '':
-                # new_name += mp3_file.tag.title + '-'
-                new_name += mp3_file.tag.title + '.mp3'
+                if mp3_file.tag.title and mp3_file.tag.title != '':
+                    if rename:
+                        new_name = mp3_file.tag.title + '.mp3'
 
-            if len(new_name) > 0:
-                # new_name = new_name[:-1] + '.mp3'
-                copyfile(_path, os.path.join(_temp_dir, new_name))
-                print new_name
-                print "\n"
-        except Exception as ex:
-            print ex
+                if len(new_name) > 0:
+                    copyfile(_path, os.path.join(_temp_dir, new_name))
+                    print new_name
+            except Exception as ex:
+                print ex
 
 
 
